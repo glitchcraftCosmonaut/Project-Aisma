2da8d719acf7510cbd518aabab4fc3f6f1c53f0d
Rhaiz <55672309+glitchcraftCosmonaut@users.noreply.github.com>
2024-03-18 12:34:01 +0700

    Puts in 3D Space

---
+++ Assets/Scripts/Unity in Action/FPSInput.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Unity in Action/FPS Input")]
+public class FPSInput : MonoBehaviour
+{
+    private CharacterController controller;
+    public float speed = 6.0f;
+
+    float gravity = -9.81f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // transform.Translate(0, speed, 0);
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        // transform.Translate(deltaX * Time.deltaTime, 0, deltaZ * Time.deltaTime);
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+
+        movement.y = gravity;
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        controller.Move(movement);
+    }
+}
---
+++ Assets/Scripts/Unity in Action/MouseLook.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public enum RotationAxes
+    {
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityX = 9.0f;
+    public float sensitivityY = 9.0f;
+
+    //limit the vertical rotation
+    public float minimumY = -45.0f;
+    public float maximumY = 45.0f;
+
+    private float verticalRot = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(axes == RotationAxes.MouseX)
+        {
+            //horizontal rotation
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+
+        }
+        else if(axes == RotationAxes.MouseY)
+        {
+            //vertical rotation
+            verticalRot -= Input.GetAxis("Mouse Y") * sensitivityY;
+
+            //limit the vertical rotation using clamp
+            verticalRot = Mathf.Clamp(verticalRot, minimumY, maximumY);
+
+            float horizontalRot = transform.localEulerAngles.y;
+
+            transform.localEulerAngles = new Vector3(verticalRot, horizontalRot, 0);
+        }
+        else
+        {
+            //both horizontal and vertical rotation
+
+            verticalRot -= Input.GetAxis("Mouse Y") * sensitivityY;
+            //limit the vertical rotation using clamp
+            verticalRot = Mathf.Clamp(verticalRot, minimumY, maximumY);
+
+            //delta is the change in the horizontal rotation
+            float delta = Input.GetAxis("Mouse X") * sensitivityX;
+            //increment rotation angle by delta
+            float horizontalRot = transform.localEulerAngles.y + delta;
+
+            transform.localEulerAngles = new Vector3(verticalRot, horizontalRot, 0);
+        }
+    }
+}
